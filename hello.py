2db9ac2216794a098344e4b90d94e575d250a4d4
okoye23 <68589984+okoye23@users.noreply.github.com>
2021-07-12 00:28:56 +0100

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,117 @@
+## 1st flowchart ##
+# name = input("Enter your name please: ")
+#
+# if name == 'Alice':
+#     print("Hi ", name )
+# else :
+#     print("you're not Alice")
+
+
+## 2nd flowchart ##
+# name = input("Please enter your name: " )
+#
+# if name == 'Alice':
+#     print("Hi",name )
+# else:
+#     print("Hello, Stranger")
+
+## 3rd flowchart ##
+# name = input("Enter name please: ")
+# if name == 'Alice':
+#     print("Hi, Alice")
+# else:
+#     age = int(input("Enter your age please: "))
+#     if age < 12:
+#         print("you are not Alice, Kiddo")
+
+
+## 4th flowchart ##
+# name = input("Enter your name: ")
+# if name == 'Alice':
+#     print("Hi, Alice")
+# else:
+#     age = int(input("Age: "))
+#     if age < 12:
+#         print("You are not Alice, Kiddo")
+#     elif age > 2000:
+#         print("Unlike you, Alice is not an undead, immortal vampire")
+#     elif age > 100:
+#         print('You are not Alice, grannie')
+#     else:
+#         print()
+
+## 5th flowchart ##
+# name = input("Enter your name: ")
+# if name == 'Alice':
+#     print("Hi, Alice")
+# else:
+#     age = int(input("Age: "))
+#     if age < 12:
+#         print("You are not Alice, Kiddo")
+#     elif age > 100 and age > 2000:
+#         print("You are not Alice, grannie")
+#     else:
+#         print("Unlike you, Alice is not an undead, immortal vampire")
+
+
+### 6th flowchart ###
+# name = input("Enter your name: ")
+# if name == 'Alice':
+#     print("Hi, Alice")
+# else:
+#     age = int(input("Enter your age: "))
+#     if age < 12:
+#         print("You are not Alice, Kiddo")
+#     else:
+#         print('You are neither Alice nor a little kid')
+
+### 7th flowchart ###
+# rain2 = "yes"
+# rain = input("is it raining: ")
+# if rain == rain2:
+#     print('Okay hold on ')
+#     if rain == rain2:
+#         rain3 = input('Do you have umberalla: ')
+#         if rain3 == rain2:
+#             print("you can go outside")
+#         else:
+#             print("Okay wait it off then")
+# else:
+#     print('Wait it out or change schedule for tomorrow')
+
+## 8th flowchart ##
+# spam = int(input("Enter a number: "))
+# if spam < 5:
+#     print("Hello, world")
+# else:
+#     spam = spam + 1
+#     print(spam)
+
+## 9th flowchart ##
+# spam = int(input("Enter any number: "))
+# if spam < 5:
+#     print('Hello, world.')
+# else:
+#     spam = spam + 1
+#     print(spam < 5)
+
+## 10th flowchart ##
+# name = input('name: ')
+# password = input("Enter password: ")
+# if name == 'Joe':
+#     print('hello joe')
+#     if password == 'swordfish':
+#         print("Access granted")
+#     else:
+#         print('Access denied')
+# else:
+#     if name != 'Joe':
+#         print('Who are you')
+
+## 11th flowchart ##
+# name = str(input('Name: '))
+# if name == "James":
+#     print(name * 5)
+# else:
+#     print('You one funny man')
+
